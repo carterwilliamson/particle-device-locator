d3c45a46f4e1d254c299fe68b27b57dfe214f617
Carter Williamson <cwilliamson@luxoft.com>
2017-08-18 09:58:15 -0700

    more config changes

--- config.sh
+++ config.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-[ -z "$API_KEY" ] && { echo "Need to set API_KEY"; exit 1; }
-sed -i -e "s/YOUR_API_KEY_HERE/${API_KEY}/g" ./config.json
+API="YOUR_API_KEY_HERE"
+if [ -z "$API_KEY" ]
+	then
+	if [ -z "$1" ]
+		then
+		echo "Need to set API_KEY env. or pass API key as arg."; exit 1;
+	else
+		API="$1"
+	fi
+else
+	API="$API_KEY"
+fi
+
+sed -i -e "s/YOUR_API_KEY_HERE/${API}/g" ./config.json
+rm ./config.json-e
